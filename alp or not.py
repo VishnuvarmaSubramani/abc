7d37f20d470bf10cc3ef9b5e1717d2c8956afacc
VishnuvarmaSubramani <51218617+VishnuvarmaSubramani@users.noreply.github.com>
2019-07-12 12:34:40 +0530

    Update alp or not.py

--- alp or not.py
+++ alp or not.py
@@ -1,5 +1,5 @@
 x=input()
-aph=['a','b','c','d','e','f','g','h','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alp=['a','b','c','d','e','f','g','h','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 if x in alp:
     print("Alphabet")
 else:
