976d1a008141c8af30de181a5db36b658c051d92
VishnuvarmaSubramani <51218617+VishnuvarmaSubramani@users.noreply.github.com>
2019-07-12 12:31:44 +0530

    Create alp or not.py

---
+++ alp or not.py
@@ -0,0 +1,6 @@
+x=input()
+aph=['a','b','c','d','e','f','g','h','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if x in alp:
+    print("Alphabet")
+else:
+    print("No")
