fc358f14fc9c208f13bc3fcb43e21b6d3cd30266
VishnuvarmaSubramani <51218617+VishnuvarmaSubramani@users.noreply.github.com>
2019-07-12 14:55:23 +0530

    Update larger no.py

--- larger no.py
+++ larger no.py
@@ -1,8 +1,8 @@
-
-x,y,z=input().split()
-if(x>=y and x>=z):
-	print(x)
-elif(y>=x and y>=z):
-	print(y)
-elif(z>=x and z>=y):
-	print(z)
+a,b,c=input().split()
+if(a>=b and a>=c):
+    largest=a
+elif(b>=a and b>=c):
+    largest=b
+else:
+    largest=c
+print(largest)
